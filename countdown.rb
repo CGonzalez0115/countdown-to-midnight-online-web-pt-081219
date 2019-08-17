8a9dc9866a1c82f24b8ee1b41bfbc5846c3f55d3
Christina Gonzalez <cmgamway@gmail.com>
2019-08-17 22:02:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ countdown_with_sleep = 10
 def countdown
   while countdown_with_sleep > 0
     countdown_with_sleep -= 10
-    puts "{countdown_with_sleep} SECOND(S)!"
+    puts "#{{}countdown_with_sleep} SECOND(S)!"
 end
 
 puts "HAPPY NEW YEAR!"
