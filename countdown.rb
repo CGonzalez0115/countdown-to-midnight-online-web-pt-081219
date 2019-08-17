b89d880cdbddfd87b243f6a8c7dd4a15cec42463
Christina Gonzalez <cmgamway@gmail.com>
2019-08-17 21:55:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
+require.pry
 countdown_with_sleep = 10
 
 def countdown
   while countdown_with_sleep > 0
     countdown_with_sleep -= 10
+  binding.pry
     puts "{countdown_with_sleep} SECOND(S)!"
 end
 
